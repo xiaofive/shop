8cab2f93346e8603f521813d1cf638d89139a8b8
xiaofive <smallfives@163.com>
2021-05-16 22:24:45 +0800

    商品中心

--- shop-product/src/main/java/com/shop/product/ProductAppApplication.java
+++ shop-product/src/main/java/com/shop/product/ProductAppApplication.java
@@ -1,5 +1,6 @@
 package com.shop.product;
 
+import com.shop.common.util.BeanConvertUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
