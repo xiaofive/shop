46ab69bda0695fc311e36ed6f06e98433bbcb0b3
smallfives@163.com <smallfives@163.com>
2021-06-07 22:15:29 +0800

    集成gateway网关服务

---
+++ shop-gateway/src/test/java/com/shop/shopgateway/ShopGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.shopgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
