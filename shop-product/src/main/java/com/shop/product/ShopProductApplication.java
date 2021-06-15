2d7a82b1f04caa034875330405f84699efb27421
smallfives@163.com <smallfives@163.com>
2021-06-15 20:44:55 +0800

    集成fengin客户端

--- shop-product/src/main/java/com/shop/product/ShopProductApplication.java
+++ shop-product/src/main/java/com/shop/product/ShopProductApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -32,6 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
         "com.shop.product.discount.mapper"
 })
 @EnableDiscoveryClient
+@EnableFeignClients
 @RestController
 public class ShopProductApplication {
 
