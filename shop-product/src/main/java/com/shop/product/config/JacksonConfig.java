c9a959cdcdcacc24a2e2076600f19ec146a9c846
smallfives@163.com <smallfives@163.com>
2021-06-25 22:24:15 +0800

    修复雪花算法生成的ID返回前段Long类型精度丢失问题

---
+++ shop-product/src/main/java/com/shop/product/config/JacksonConfig.java
@@ -0,0 +1,36 @@
+package com.shop.product.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * 雪花算法生成的ID在返回给前端之后和生成的不一样，精度问题
+ * 通过添加一个全局配置来使long类型转为JSON中的string类型
+ * long类型继承的是number类，而number类型精度为16位，而雪花算法生成的id为19位，因此会导致进度丢失
+ * <p>
+ * Author: wang Y
+ * Date: 2021-06-25
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        // 全局配置序列化返回 JSON 处理
+        SimpleModule simpleModule = new SimpleModule();
+        //JSON Long ==> String
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        return objectMapper;
+    }
+
+}
